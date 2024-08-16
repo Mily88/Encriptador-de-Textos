cbbd93532b6ac6833a6ca8e92c4538ca3a40b2cf
Mily <notomopepsicola88@gmail.com>
2024-08-16 02:06:27 -0600

    Diseño y funcionamiento de la pagina 0.8v

---
+++ index.js
@@ -0,0 +1,27 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let candado = document.getElementById("candado");
+
+
+    let textoCifrado = texto
+                .replace(/e/gi, "enter")
+                .replace(/i/gi, "imes")
+                .replace(/a/gi, "ai")
+                .replace(/o/gi, "ober")
+                .replace(/u/gi, "ufat");
+                
+    if (texto.lenght != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        candado.src = "./imagenes/candado3.png";
+    } else {
+        candado.src = "./imagenes/candado4.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }               
+}
+
