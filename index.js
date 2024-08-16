ca483235befb2137e33d17bc13f440c0beb2e738
Mily <notomopepsicola88@gmail.com>
2024-08-16 02:25:18 -0600

    Se agrego la funcion desencriptar

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function encriptar(){
 
 
     let textoCifrado = texto
-                .replace(/e/gi, "enter")
-                .replace(/i/gi, "imes")
-                .replace(/a/gi, "ai")
-                .replace(/o/gi, "ober")
-                .replace(/u/gi, "ufat");
-                
+     .replace(/e/gi, "enter")
+     .replace(/i/gi, "imes")
+     .replace(/a/gi, "ai")
+     .replace(/o/gi, "ober")
+     .replace(/u/gi, "ufat");
+              
     if (texto.lenght != 0){
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
@@ -23,5 +23,31 @@ function encriptar(){
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         alert("Debes ingresar algún texto");
     }               
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let candado = document.getElementById("candado");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+    
+    if (texto.lenght != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        candado.src = "./imagenes/candado2.png";
+    } else {
+        candado.src = "./imagenes/candado2.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
 }
 
